e2dd03dbd3cdc727ce148f933ee94acdd1c9ba32
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2022-01-12 15:05:36 -0500

    Added NFT smart contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT is ERC721URIStorage, Ownable {
+    uint256 public currentSupply;
+    mapping(uint256 => string) private _tokenURIs;
+    mapping(uint256 => address) private _artists;
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {
+        currentSupply = 0;
+    }
+
+    function mint(string memory _tokenURI) public onlyOwner returns (uint256) {
+        uint256 tokenId = currentSupply + 1;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        _artists[tokenId] = msg.sender;
+
+        currentSupply++;
+        return tokenId;
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,70 @@
+import { task } from "hardhat/config";
+import Web3 from "web3";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+task("deploy", "Deploy the smart contracts", async (args, hre) => {
+  const TOKEN_NAME = process.env.TOKEN_NAME ?? "Test NFT";
+  const TOKEN_SYMBOL = process.env.TOKEN_SYMBOL ?? "TNFT";
+
+  console.log(`Deploying ${TOKEN_NAME} with symbol ${TOKEN_SYMBOL}`);
+
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const smartContract = await NFT.deploy(TOKEN_NAME, TOKEN_SYMBOL);
+  await smartContract.deployed();
+
+  await txWait(smartContract.deployTransaction.hash, 5, (hre.network.config as any).url);
+
+  console.log(`Deployed contact at ${smartContract.address}`);
+
+  await hre.run("verify:verify", {
+    address: smartContract.address,
+    constructorArguments: [TOKEN_NAME, TOKEN_SYMBOL],
+  }).catch(e => console.log(e.message))
+});
+
+async function txWait(txHash: string, confirmations: number, networkUrl: string) {
+  const web3 = new Web3(networkUrl) as any;
+  const tx = await web3.eth.getTransaction(txHash);
+  let currentBlock = await web3.eth.getBlockNumber();
+  while (currentBlock - tx.blockNumber < confirmations) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    currentBlock = await web3.eth.getBlockNumber();
+  }
+}
+
+export default {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    mumbai_dev: {
+      url: process.env.ALCHEMY_DEV_URL ?? "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY_DEV]
+    },
+    matic_prod: {
+      url: process.env.ALCHEMY_PROD_URL,
+      accounts: [process.env.PRIVATE_KEY_PROD],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_KEY,
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
