a175bf960ea7d9f0393111c12d115ed77e5577ae
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2022-05-22 19:45:39 -0400

    Contractv5
    - Adds deployer in ApprovedForAll
    - Adds ability to reveal NFT on transfer

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFT is ERC721URIStorage, Ownable {
     uint256 public currentSupply;
-    mapping(address => bool) public minters;
+    address deployer;
     mapping(string => uint256) private uris;
+    bool revealOnTransfer = false;
 
     event SimulatedTransfer(
         address indexed from,
@@ -21,14 +22,11 @@ contract NFT is ERC721URIStorage, Ownable {
         address _owner
     ) ERC721(_name, _symbol) Ownable(_owner) {
         currentSupply = 1;
-        minters[msg.sender] = true;
+        deployer = msg.sender;
     }
 
     modifier onlyAuthorized() {
-        require(
-            minters[msg.sender] == true || msg.sender == owner(),
-            "Not allowed"
-        );
+        require(msg.sender == deployer || msg.sender == owner(), "Not allowed");
         _;
     }
 
@@ -52,12 +50,17 @@ contract NFT is ERC721URIStorage, Ownable {
         return currentSupply - 1;
     }
 
-    function addMinter(address minter) public onlyOwner {
-        minters[minter] = true;
-    }
-
-    function removeMinter(address minter) public onlyOwner {
-        minters[minter] = false;
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (revealOnTransfer && ownerOf(tokenId) == owner()) {
+            return "";
+        }
+        return super.tokenURI(tokenId);
     }
 
     function isApprovedForAll(address _owner, address _operator)
@@ -69,11 +72,16 @@ contract NFT is ERC721URIStorage, Ownable {
         // if Rarible or OpenSea's ERC721 Proxy Address is detected, auto-return true
         if (
             _operator == address(0xd47e14DD9b98411754f722B4c4074e14752Ada7C) ||
-            _operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)
+            _operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE) ||
+            _operator == deployer
         ) {
             return true;
         }
 
         return ERC721.isApprovedForAll(_owner, _operator);
+    }
+
+    function setRevealOnTransfer(bool _revealOnTransfer) public onlyAuthorized {
+        revealOnTransfer = _revealOnTransfer;
     }
 }
